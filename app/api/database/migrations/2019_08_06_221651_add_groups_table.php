34552e70e27da85204247195398076948368c364
苑田征弥 <other0315@gmail.com>
2019-08-06 23:50:06 +0900

    (migration) add groups table

---
+++ app/api/database/migrations/2019_08_06_221651_add_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Kalnoy\Nestedset\NestedSet;
+
+class AddGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 32);
+            $table->nestedSet();
+            $table->timestamps();
+            $table->dateTime('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('groups');
+    }
+}
