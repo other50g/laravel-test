bbb1bac29b380090eb41fd29f0d7f0ec044466a3
苑田征弥 <other0315@gmail.com>
2019-08-07 19:34:51 +0900

    （マイグレーション）ユーザーとグループのリレーションを作成

---
+++ app/api/database/migrations/2019_08_07_183045_add_group_id_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddGroupIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->bigInteger('group_id')->after('email_verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('group_id');
+        });
+    }
+}
